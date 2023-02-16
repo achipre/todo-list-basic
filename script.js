65fe82835cccf3d9afcef20f91e7d543119aabb5
Alex Chipre <alechipre@gmail.com>
2023-02-15 19:00:17 -0500

    Class 4 accomplish

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Selection: Button
+( () => {
+  // Selection: Button
 const btn = document.querySelector('[data-form-btn]');
 // Selection: Input
 const input = document.querySelector('[data-form-input]');
@@ -11,16 +12,31 @@ input.focus();
 const crearTarea = (e) => {
   // evitar que se recaergue el formulario;
   e.preventDefault();
-  // crear Elemento il en el ul: con el data-list por cada click
+  // crear Elemento il
   const task = document.createElement('li');
+  // crear Elemento div
+  const taskContent = document.createElement('div');
+  // crear Elemento span
+  const titleTask = document.createElement('span');
+  
+  // Agregar el contenido generado en el ul 
+  taskContent.appendChild(checkComplete());
   // Agregar la clase card a la lista li recien creada
   task.classList.add('card');
   // Crear contenido con el input ingresado
-  const content = `<div><i class="far fa-check-square icon"></i><span class="task">${input.value}</span></div><i class="fas fa-trash-alt trashIcon icon"></i>`;
+  titleTask.classList.add('task');
+  // Crear contenido con el input ingresado
+  const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`;
   // Agregar la tarea generada al HTML
   task.innerHTML = content;
+  // Agregar la tarea generada al HTML
+  titleTask.innerText = input.value;
+  // Agregar el contenido e=generado en el ul 
+  taskContent.appendChild(titleTask);
   // Agregar el contenido e=generado en el ul 
   list.appendChild(task);
+  // Agregar el contenido e=generado en el ul 
+  task.appendChild(taskContent);
   // Borrar el input al dar click
   input.value = '';
   // Enfoca al inputa al dar click en boton
@@ -28,3 +44,18 @@ const crearTarea = (e) => {
 };
 
 btn.addEventListener('click', crearTarea);
+
+const checkComplete = () => {
+  const i = document.createElement('i');
+  i.classList.add('far', 'fa-check-square', 'icon');
+  i.addEventListener('click', completeTask)
+
+  return i
+};
+const completeTask = (e) => {
+  const elemet = e.target;
+  elemet.classList.toggle('fas')
+  elemet.classList.toggle('completeIcon')
+  elemet.classList.toggle('far')
+};
+})();
